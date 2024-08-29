a0290ec3c20e7b9682ee4e73d13dcaf0474263e7
gerhardadler <gerhardskartveit@gmail.com>
2024-08-29 19:39:24 +0200

    curves improvements

--- src/curves.js
+++ src/curves.js
@@ -1,3 +1,26 @@
+class Point {
+  constructor(x, y, getSvgRect) {
+    this.x = x;
+    this.y = y;
+    this.getSvgRect = getSvgRect;
+  }
+
+  get actualX() {
+    return this.x * this.getSvgRect().width;
+  }
+  get actualY() {
+    return this.getSvgRect().height - this.y * this.getSvgRect().height;
+  }
+
+  static fromActual(x, y, getSvgRect) {
+    return new Point(
+      x / getSvgRect().width,
+      1 - y / getSvgRect().height,
+      getSvgRect
+    );
+  }
+}
+
 class Curves {
   constructor(
     svg,
@@ -16,10 +39,12 @@ class Curves {
 
     this.svg.addEventListener("mousedown", this.boundOnMouseDown);
     this.svg.addEventListener("mousemove", this.boundOnMouseMove);
-    this.svgRect = svg.getBoundingClientRect();
 
-    this.points = initialPoints;
-    this.closestPoint = [0, 0];
+    this.points = initialPoints.map(
+      ([x, y]) => new Point(x, y, this.getBoundSvgRect())
+    );
+
+    this.closestPoint = new Point(0, 0, this.getBoundSvgRect());
     this.closestPointIndex = 0;
     this.selectedPoint = null;
     this.selectedPointIndex = null;
@@ -27,32 +52,55 @@ class Curves {
     this.update();
   }
 
+  getMousePosition(e) {
+    return Point.fromActual(
+      e.clientX - this.getSvgRect().left,
+      e.clientY - this.getSvgRect().top,
+      this.getBoundSvgRect()
+    );
+  }
+
+  getSvgRect() {
+    return this.svg.getBoundingClientRect();
+  }
+
+  getBoundSvgRect() {
+    return this.getSvgRect.bind(this);
+  }
+
   update() {
-    this.points.sort(([xa, _], [xb, __]) => xa > xb);
+    this.points.sort((pointA, pointB) => pointA.x > pointB.x);
     this.drawCircles();
     this.drawPath();
   }
 
   getPoints() {
-    const listPrefix = this.points[0][0] !== 0 ? [[0, this.points[0][1]]] : [];
+    const listPrefix =
+      this.points[0].x !== 0
+        ? [new Point(0, this.points[0].y, this.getBoundSvgRect())]
+        : [];
     const listSuffix =
-      this.points[this.points.length - 1][0] !== 1
-        ? [[1, this.points[this.points.length - 1][1]]]
+      this.points[this.points.length - 1].x !== 1
+        ? [
+            new Point(
+              1,
+              this.points[this.points.length - 1].y,
+              this.getBoundSvgRect()
+            ),
+          ]
         : [];
     return listPrefix.concat(this.points, listSuffix);
   }
 
   drawCircles() {
     this.svg.querySelectorAll("circle").forEach((circle) => circle.remove());
-    this.points.forEach(([x, y]) => {
-      const actualX = x * this.svgRect.width;
-      const actualY = y * this.svgRect.height;
+    this.points.forEach((point) => {
       const newCircle = document.createElementNS(
         "http://www.w3.org/2000/svg",
         "circle"
       );
-      newCircle.setAttribute("cx", actualX);
-      newCircle.setAttribute("cy", actualY);
+      newCircle.setAttribute("cx", point.actualX);
+      newCircle.setAttribute("cy", point.actualY);
       newCircle.setAttribute("r", this.circleRadius);
       newCircle.setAttribute("style", this.circleStyle);
       this.svg.appendChild(newCircle);
@@ -65,43 +113,38 @@ class Curves {
       path.setAttribute("style", this.pathStyle);
       this.svg.prepend(path);
     }
-    let d = `M ${0} ${this.points[0][1] * this.svgRect.width} `;
-    this.points[0][1];
-    this.points.forEach(([x, y], i) => {
-      const actualX = x * this.svgRect.width;
-      const actualY = y * this.svgRect.height;
-      d += `L ${parseFloat(actualX)} ${parseFloat(actualY)} `;
+    let d = `M ${0} ${this.points[0].actualY} `;
+    this.points.forEach((point) => {
+      d += `L ${parseFloat(point.actualX)} ${parseFloat(point.actualY)} `;
     });
-    d += `L ${this.svgRect.width} ${
-      this.points[this.points.length - 1][1] * this.svgRect.height
+    d += `L ${this.getSvgRect().width} ${
+      this.points[this.points.length - 1].actualY
     }`;
     path.setAttribute("d", d);
   }
 
   onMouseMove(e) {
-    let actualX = e.clientX - this.svgRect.left;
-    let actualY = e.clientY - this.svgRect.top;
-
-    let x = actualX / this.svgRect.width;
-    let y = actualY / this.svgRect.height;
-
+    const mousePosition = this.getMousePosition(e);
     let closestDistance = Infinity;
 
-    this.points.forEach(([px, py], i) => {
+    this.points.forEach((point, i) => {
       // Calculate the distance from the target point
-      const distance = Math.sqrt(Math.pow(px - x, 2) + Math.pow(py - y, 2));
+      const distance = Math.sqrt(
+        Math.pow(point.x - mousePosition.x, 2) +
+          Math.pow(point.y - mousePosition.y, 2)
+      );
 
       // Check if this point is closer than the previous closest
       if (distance < closestDistance) {
         closestDistance = distance;
-        this.closestPoint = [px, py];
+        this.closestPoint = point;
         this.closestPointIndex = i;
       }
     });
 
     if (
-      Math.abs(this.closestPoint[0] - x) < 0.03 &&
-      Math.abs(this.closestPoint[1] - y) < 0.03
+      Math.abs(this.closestPoint.x - mousePosition.x) < 0.03 &&
+      Math.abs(this.closestPoint.y - mousePosition.y) < 0.03
     ) {
       this.svg.style.cursor = "move";
       this.selectedPoint = this.closestPoint;
@@ -119,38 +162,37 @@ class Curves {
         this.update();
         return;
       }
-      const prevX = this.points[this.selectedPointIndex - 1]?.[0] ?? 0;
+      const prevX = this.points[this.selectedPointIndex - 1]?.x ?? 0;
       const nextX =
-        this.points[this.selectedPointIndex + 1]?.[0] ?? this.svgRect.width;
+        this.points[this.selectedPointIndex + 1]?.x ??
+        this.getSvgRect().width / this.getSvgRect().width;
       const onMouseMove = (e) => {
-        let actualX = e.clientX - this.svgRect.left;
-        let actualY = e.clientY - this.svgRect.top;
+        const mousePosition = this.getMousePosition(e);
 
-        let x = actualX / this.svgRect.width;
-        let y = actualY / this.svgRect.height;
+        mousePosition.x = Math.max(prevX, Math.min(nextX, mousePosition.x));
+        mousePosition.y = Math.max(
+          0,
+          Math.min(
+            this.getSvgRect().height / this.getSvgRect().height,
+            mousePosition.y
+          )
+        );
 
-        x = Math.max(prevX, Math.min(nextX, x));
-        y = Math.max(0, Math.min(this.svgRect.height / this.svgRect.height, y));
-
-        this.points[this.selectedPointIndex] = [x, y];
+        this.points[this.selectedPointIndex] = mousePosition;
         this.update();
       };
       const onMouseUp = () => {
         window.removeEventListener("mousemove", onMouseMove);
         this.svg.addEventListener("mousemove", this.boundOnMouseMove);
-        this.svg.removeEventListener("mouseup", onMouseUp);
+        window.removeEventListener("mouseup", onMouseUp);
       };
       this.svg.removeEventListener("mousemove", this.boundOnMouseMove);
       window.addEventListener("mousemove", onMouseMove);
-      this.svg.addEventListener("mouseup", onMouseUp);
+      window.addEventListener("mouseup", onMouseUp);
     } else {
-      let actualX = e.clientX - this.svgRect.left;
-      let actualY = e.clientY - this.svgRect.top;
+      const mousePosition = this.getMousePosition(e);
 
-      let x = actualX / this.svgRect.width;
-      let y = actualY / this.svgRect.height;
-
-      this.points.push([x, y]);
+      this.points.push(mousePosition);
       this.update();
     }
   }
